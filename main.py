af7747733ef9710e7469fc6497c6dce95a184366
aoyouer <whaojie06@gmail.com>
2021-12-21 01:06:25 +0800

    在无头模式下异常，权限不足，在图形化模式下正常

--- main.py
+++ main.py
@@ -13,7 +13,10 @@ chrome_options.add_experimental_option('prefs', {
     "profile.default_content_settings.geolocation": 1,
     "profile.default_content_settings.popups": 1
 })
-chrome_options.add_argument('--headless')
+# chrome_options.add_argument('--headless')
+chrome_options.add_argument("--window-size=1920, 1080")
+chrome_options.add_argument(
+    "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36")
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--no-sandbox')
 
@@ -26,24 +29,18 @@ params = {
     "accuracy": 100
 }
 # client.execute_cdp_cmd("Page.setGeolocationOverride", params)
-# client.get("https://google.com/search?q=1")
-# client.get('https://www.google.com/maps')
 # client.get('https://www.infobyip.com/browsergeolocation.php')
 client.execute_cdp_cmd("Emulation.setGeolocationOverride", params)
-# client.get("https://maps.google.com")
-client.get('https://browserleaks.com/geo')
-
-width = client.execute_script("return document.documentElement.scrollWidth")
-height = client.execute_script("return document.documentElement.scrollHeight")
-client.set_window_size(width, height)
-print(width, height)
-# time.sleep(3)
-
+# client.get('https://browserleaks.com/geo')
+client.get("https://google.com/search?q=1")
+# width = client.execute_script("return document.documentElement.scrollWidth")
+# height = client.execute_script("return document.documentElement.scrollHeight")
+# client.set_window_size(width, height)
+# print(width, height)
 element = WebDriverWait(client, 10).until(
-    EC.presence_of_element_located((By.ID, "geo_update_button")))  # added
+    EC.presence_of_element_located((By.TAG_NAME, "update-location")))  # added
 element.click()  # added
-
-
+time.sleep(4)
 # client.find_element(By.TAG_NAME, 'update-location').click()
 # client.find_element(By.ID, 'geo_update_button').click()
 
